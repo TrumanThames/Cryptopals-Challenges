a080d46993e8c2a5dfebb66388319b47a2403224
Jaun <jaun@example.com>
2020-10-15 09:03:12 -0500

    Added

---
+++ pkcs_pad.py
@@ -0,0 +1,21 @@
+import sys
+
+def pad(t, size=16):
+    l = len(t)
+    a = l % (size)
+    if a == 0:
+        return t
+    return t+('\x04'*(size-a)).encode()
+
+if __name__ == "__main__":
+    if (len(sys.argv) <= 1):
+        print("need at least one argument, the bytes to be padded")
+        print("optional second argument, the block size to pad to")
+        sys.exit()
+    text = sys.argv[1]
+    if (len(sys.argv) >=3):
+        block_size = int(sys.argv[2])
+    else:
+        block_size = 16
+    ptext = pad(text.encode(), block_size)
+    print(ptext)
